af3f71e8e8a3e31852544ac10fb8276dfb089ef9
developomp <developomp@protonmail.com>
2024-10-06 08:19:18 +0900

    add nautilus dconf settings

--- home.nix
+++ home.nix
@@ -89,6 +89,26 @@
       hinting = "full";
     };
 
+    "org/gnome/nautilus/preferences" = {
+      default-folder-viewer = "icon-view";
+      search-filter-time-type = "last_modified";
+      search-view = "list-view";
+      show-create-link = true;
+      show-hidden-files = true;
+    };
+
+    "org/gnome/nautilus/compression" = {
+      default-compression-format = "zip";
+    };
+
+    "org/gnome/nautilus/icon-view" = {
+      default-zoom-level = "small";
+    };
+
+    "org/gnome/nautilus/list-view" = {
+      default-zoom-level = "larger";
+    };
+
     "org/gnome/shell" = {
       disable-user-extensions = false;
       enabled-extensions = [
