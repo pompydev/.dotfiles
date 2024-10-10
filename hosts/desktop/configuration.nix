b95558e7487ce8f3f4c964b984547df9cae2d250
developomp <developomp@protonmail.com>
2024-10-11 07:16:06 +0900

    remove redundant config

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -225,32 +225,6 @@
   };
 
   # System
-  time.timeZone = "Asia/Seoul";
-  i18n = {
-    inputMethod = {
-      enable = true;
-      type = "ibus";
-      ibus.engines = with pkgs.ibus-engines; [
-        hangul # Korean
-        anthy # Japanese
-        pinyin # Chinese
-        uniemoji # Emoji
-      ];
-    };
-
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
-  };
   boot.loader.systemd-boot.enable = true;
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
