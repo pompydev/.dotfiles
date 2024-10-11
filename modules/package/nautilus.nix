383bf238b437d902f102587fb27b9cc345f49fe8
developomp <developomp@protonmail.com>
2024-10-11 09:15:51 +0900

    fix deprecated option

--- modules/package/nautilus.nix
+++ modules/package/nautilus.nix
@@ -7,7 +7,7 @@
   };
 
   environment = {
-    sessionVariables.NAUTILUS_4_EXTENSION_DIR = "${pkgs.gnome.nautilus-python}/lib/nautilus/extensions-4";
+    sessionVariables.NAUTILUS_4_EXTENSION_DIR = "${pkgs.nautilus-python}/lib/nautilus/extensions-4";
     pathsToLink = [
       "/share/nautilus-python/extensions"
     ];
