c1af09562379d16da77d9cc5a25bfb58ed3b1ede
developomp <developomp@protonmail.com>
2024-10-11 17:21:04 +0900

    add devenv cache

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -32,10 +32,12 @@
       "https://nix-community.cachix.org"
       "https://cache.nixos.org/"
       "https://cuda-maintainers.cachix.org"
+      "https://devenv.cachix.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     ];
 
     experimental-features = [
