5cac9525315f5d0fd2f71e5f11932c739f5a00da
developomp <developomp@protonmail.com>
2024-10-21 11:28:29 +0900

    refactor out nixos module `nix`

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -22,6 +22,7 @@
     ../../modules/nixos/locale.nix
     ../../modules/nixos/mongodb-compass.nix
     ../../modules/nixos/nautilus.nix
+    ../../modules/nixos/nix.nix
     ../../modules/nixos/obs.nix
     ../../modules/nixos/osu-lazer-bin.nix
     ../../modules/nixos/otd.nix
@@ -30,27 +31,6 @@
     inputs.home-manager.nixosModules.default
   ];
   nixpkgs.config.allowUnfree = true;
-  nix.extraOptions = ''
-    trusted-users = root pomp
-  ''; # allow pomp to manage nix store
-  nix.settings = {
-    substituters = [
-      "https://nix-community.cachix.org"
-      "https://cache.nixos.org/"
-      "https://cuda-maintainers.cachix.org"
-      "https://devenv.cachix.org"
-    ];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-    ];
-
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-  };
   home-manager = {
     users = {
       pomp = import ./home.nix;
