88b756d836339820e344f2247171e1ccfea3d700
developomp <developomp@protonmail.com>
2024-10-26 20:53:50 +0900

    update input related dconf settings

--- modules/home-manager/dconf-input.nix
+++ modules/home-manager/dconf-input.nix
@@ -2,56 +2,57 @@
 
 with lib.hm.gvariant;
 {
-  dconf.settings."org/gnome/desktop/input-sources" = {
-    mru-sources = [
-      (mkTuple [
-        "ibus"
-        "hangul"
-      ])
-      (mkTuple [
-        "ibus"
-        "anthy"
-      ])
-      (mkTuple [
-        "ibus"
-        "pinyin"
-      ])
-      (mkTuple [
-        "ibus"
-        "uniemoji"
-      ])
-    ];
-    sources = [
-      (mkTuple [
-        "ibus"
-        "hangul"
-      ])
-      (mkTuple [
-        "ibus"
-        "anthy"
-      ])
-      (mkTuple [
-        "ibus"
-        "pinyin"
-      ])
-      (mkTuple [
-        "ibus"
-        "uniemoji"
-      ])
-    ];
-    xkb-options = [
-      "terminate:ctrl_alt_bksp"
-      "compose:ralt"
-      "lv3:switch"
-    ];
-  };
+  dconf.settings = {
+    "org/gnome/desktop/input-sources" = {
+      mru-sources = [
+        (mkTuple [
+          "ibus"
+          "hangul"
+        ])
+        (mkTuple [
+          "ibus"
+          "anthy"
+        ])
+        (mkTuple [
+          "ibus"
+          "pinyin"
+        ])
+        (mkTuple [
+          "ibus"
+          "uniemoji"
+        ])
+      ];
+      sources = [
+        (mkTuple [
+          "ibus"
+          "hangul"
+        ])
+        (mkTuple [
+          "ibus"
+          "anthy"
+        ])
+        (mkTuple [
+          "ibus"
+          "pinyin"
+        ])
+        (mkTuple [
+          "ibus"
+          "uniemoji"
+        ])
+      ];
+    };
 
-  dconf.settings."org/freedesktop/ibus/engine/hangul" = {
-    auto-reorder = false;
-    hangul-keyboard = "2";
-    hanja-keys = "Hangul_Hanja";
-    initial-input-mode = "latin";
-    switch-keys = "Alt_R";
-    word-commit = false;
+    "org/gnome/desktop/wm/keybindings" = {
+      close = [ "<Shift><Control>q" ];
+    };
+
+    "org/freedesktop/ibus/engine/hangul" = {
+      auto-reorder = false;
+      hangul-keyboard = "2";
+      hanja-keys = "Hangul_Hanja";
+      initial-input-mode = "latin";
+      switch-keys = "Multi_key";
+      word-commit = false;
+    };
   };
 }
