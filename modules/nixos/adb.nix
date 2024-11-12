3260e0cce0a203c4e62601b0807aa9a2e0feba5b
developomp <developomp@protonmail.com>
2024-11-12 18:32:46 +0900

    improve module `adb`

--- modules/nixos/adb.nix
+++ modules/nixos/adb.nix
@@ -1,7 +1,11 @@
+# https://nixos.wiki/wiki/Android
 { pkgs, ... }:
 
 {
   programs.adb.enable = true;
-  users.users.pomp.extraGroups = [ "adbusers" ];
+  users.users.pomp.extraGroups = [
+    "adbusers"
+    "kvm"
+  ];
   services.udev.packages = [ pkgs.android-udev-rules ];
 }
