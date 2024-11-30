98e3615bc33acb90e8948c7bc5d787afc428faa7
developomp <developomp@protonmail.com>
2024-11-30 16:25:43 +0900

    improve fonts

--- modules/nixos/fonts.nix
+++ modules/nixos/fonts.nix
@@ -1,8 +1,32 @@
 { pkgs, ... }:
 
+let
+  # https://github.com/Soft/nix-google-fonts-overlay
+  fontsOverlay = import (
+    builtins.fetchTarball {
+      url = "https://github.com/Soft/nix-google-fonts-overlay/archive/e089b8ef1c5ec95cd0a246311756d83f9f11b489.tar.gz";
+      sha256 = "sha256:067ws05gk60al1ihiwwan2vbfjlm61d6m50qk9smb89fkmfi4z4n";
+    }
+  );
+in
 {
-  environment.systemPackages = with pkgs; [
-    # Fonts
+  nixpkgs.overlays = [
+    fontsOverlay
+  ];
+
+  fonts.fontDir.enable = true;
+  fonts.packages = with pkgs; [
+    # Windows Fonts
+    corefonts
+    vistafonts
+    liberation_ttf
+
+    # Google fonts
+    google-fonts-audiowide
+
+    # https://github.com/NixOS/nixpkgs/tree/master/pkgs/data/fonts/nerd-fonts
+    nerd-fonts.terminess-ttf
+
     meslo-lgs-nf
     ubuntu-sans-mono
     noto-fonts
@@ -11,7 +35,6 @@
     noto-fonts-color-emoji
     noto-fonts-emoji
     noto-fonts-extra
-    nerd-fonts.terminess-ttf
     nanum
   ];
 }
