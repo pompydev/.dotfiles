af82c82218bb8e72cf169f9762f356fb51e83f0e
developomp <developomp@protonmail.com>
2024-12-01 13:39:00 +0900

    remove hack fix not relevant anymore

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -171,8 +171,6 @@
   };
 
   # System
-  systemd.network.wait-online.enable = false;
-  boot.initrd.systemd.network.wait-online.enable = false;
   boot.loader.systemd-boot.enable = true;
   boot.kernelParams = [
     "quiet"
