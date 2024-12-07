7418c54113b6bb4ca53130226cc1cc4fd3e9d179
developomp <developomp@protonmail.com>
2024-12-07 21:11:19 +0900

    improve osu performance

--- modules/home-manager/osu.nix
+++ modules/home-manager/osu.nix
@@ -8,11 +8,12 @@
   home.file.".local/share/applications/osu.desktop".text = ''
     [Desktop Entry]
     Name=osu!
+    # https://download.nvidia.com/XFree86/Linux-x86_64/525.78.01/README/openglenvvariables.html
     # https://github.com/PipeWire/pipewire?tab=readme-ov-file#usage
     # osu! uses a sample rate of 44.1kHz instead of the usual 48kHz.
     # Setting PIPEWIRE_LATENCY=16/48000 does not force the game to use 48kHz sample rate.
     # values below 8 causes issue
-    Exec=env PIPEWIRE_LATENCY=16/48000 gamemoderun osu! %U
+    Exec=env PIPEWIRE_LATENCY=16/48000 __GL_MaxFramesAllowed=1 __GL_SYNC_TO_VBLANK=0 gamemoderun osu! %U
     Categories=Game
     Comment=A free-to-win rhythm game. Rhythm is just a *click* away!
     SingleMainWindow=true
