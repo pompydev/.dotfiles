79d5425b3f8bf1980c08cf5221d8102376ce6ebe
developomp <developomp@protonmail.com>
2024-12-28 10:39:30 +0900

    enable GNOME remote desktop

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -27,6 +27,7 @@
     gnome-tweaks
   ];
 
+  services.gnome.gnome-remote-desktop.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.gnome.core-utilities.enable = false;
