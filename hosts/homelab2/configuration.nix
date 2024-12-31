f39f038a2b1b7e0417ad0d0a2ca250233dd45e74
developomp <developomp@protonmail.com>
2025-01-01 08:55:34 +0900

    enable SSH on homelab2

--- hosts/homelab2/configuration.nix
+++ hosts/homelab2/configuration.nix
@@ -7,6 +7,7 @@
 {
   imports = [
     ./hardware-configuration.nix
+    ./modules/ssh.nix
     ../../modules/nixos/locale.nix
     ../../modules/nixos/fonts.nix
     ../../modules/nixos/gnome-remote-desktop.nix
@@ -130,5 +131,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
-
 }
---
+++ hosts/homelab2/modules/ssh.nix
@@ -0,0 +1,26 @@
+{ ... }:
+
+{
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = null;
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "prohibit-password";
+    };
+  };
+
+  services.fail2ban = {
+    enable = true;
+    maxretry = 5; # Ban IP after 5 failures
+    bantime = "24h"; # Ban IPs for one day on the first ban
+    bantime-increment = {
+      enable = true; # Enable increment of bantime after each violation
+      multipliers = "1 2 4 8 16 32 64";
+      maxtime = "168h"; # Do not ban for more than 1 week
+      overalljails = true; # Calculate the bantime based on all the violations
+    };
+  };
+}
