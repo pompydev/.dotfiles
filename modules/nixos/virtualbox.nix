621768bb198170eea96c3c826c92bdeefc651733
developomp <developomp@protonmail.com>
2025-02-22 09:16:04 +0900

    fix virtualbox not working

--- modules/nixos/virtualbox.nix
+++ modules/nixos/virtualbox.nix
@@ -1,9 +1,8 @@
-{ pkgs, user, ... }:
+{ user, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    virtualbox
-  ];
+  # https://discourse.nixos.org/t/issue-with-virtualbox-in-24-11/57607/2
+  boot.kernelParams = [ "kvm.enable_virt_at_load=0" ];
 
   virtualisation.virtualbox.host.enable = true;
   virtualisation.virtualbox.host.enableExtensionPack = true;
