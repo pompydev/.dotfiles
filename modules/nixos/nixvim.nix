5149fd26e2db5f3d400ffbf1d3916086c2d8c96d
pompydev <pompydev@proton.me>
2025-03-14 09:28:56 +0900

    update nixvim config

--- modules/nixos/nixvim.nix
+++ modules/nixos/nixvim.nix
@@ -1,12 +1,39 @@
 { ... }:
 
 {
-  # https://github.com/nix-community/nixvim
+  # https://nix-community.github.io/nixvim/
   programs.nixvim = {
     enable = true;
     colorschemes.onedark.enable = true;
 
-    keymaps = [ ];
+    opts = {
+      number = true;
+      relativenumber = true;
+      shiftwidth = 2;
+    };
+
+    keymaps = [
+      {
+        key = "<C-P>";
+        action = "<cmd>Telescope find_files<cr>";
+      }
+      {
+        key = "<leader>gg";
+        action = "<cmd>LazyGit<cr>";
+      }
+      {
+        key = "<leader>p";
+        action = "<cmd>Gitsigns preview_hunk<cr>";
+      }
+      {
+        key = "<C-T>";
+        action = "<cmd>NvimTreeToggle<cr>";
+      }
+      {
+        key = "<C-S>";
+        action = "<cmd>w<cr>";
+      }
+    ];
 
     plugins = {
       lsp = {
@@ -17,9 +44,15 @@
             installCargo = true;
             installRustc = true;
           };
+	  gopls.enable = true;
           nixd.enable = true;
         };
       };
+      lsp-format = {
+        enable = true;
+      };
+      lsp-lines.enable = true;
+
       telescope = {
         enable = true;
         extensions = {
@@ -27,8 +60,52 @@
           fzf-native.enable = true;
         };
       };
-      treesitter.enable = true;
+
+      floaterm = {
+        enable = true;
+        settings = {
+          keymap_kill = "<Leader>fx";
+          keymap_new = "<Leader>ft";
+          keymap_next = "<Leader>fn";
+          keymap_prev = "<Leader>fp";
+          keymap_toggle = "<Leader>t";
+        };
+      };
+
+      nvim-tree = {
+        enable = true;
+        hijackCursor = true;
+        hijackDirectories.enable = true;
+      };
+
+      dashboard = {
+        enable = true;
+        settings = {
+          config = {
+            shortcut = [ ];
+            header = [
+              "   ██████╗  ██████╗ ███╗   ███╗██████╗ "
+              "   ██╔══██╗██╔═══██╗████╗ ████║██╔══██╗"
+              "   ██████╔╝██║   ██║██╔████╔██║██████╔╝"
+              "   ██╔═══╝ ██║   ██║██║╚██╔╝██║██╔═══╝ "
+              "   ██║     ╚██████╔╝██║ ╚═╝ ██║██║     "
+              "   ╚═╝      ╚═════╝ ╚═╝     ╚═╝╚═╝     "
+            ];
+            footer = [ "" ];
+          };
+        };
+      };
+
+      trouble.enable = true;
+      lazygit.enable = true;
+      gitsigns.enable = true;
+      airline.enable = true;
       bufferline.enable = true;
+      dropbar.enable = true;
+      markview.enable = true;
+      scrollview.enable = true;
+      neoscroll.enable = true;
+      treesitter.enable = true;
       web-devicons.enable = true;
     };
   };
