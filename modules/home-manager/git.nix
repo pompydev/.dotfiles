3a4050f3435f93ce04610d5359f6247095c3180d
pompydev <pompydev@proton.me>
2025-03-25 19:51:07 +0900

    improve git config

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -16,17 +16,28 @@
       key = "D3932F82A0667A3B";
     };
     extraConfig = {
+      # https://blog.gitbutler.com/how-git-core-devs-configure-git/
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+      init.defaultBranch = "master";
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
       push = {
         autoSetupRemote = true;
+        followTags = true;
       };
-
-      pull = {
-        rebase = true;
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
       };
-
-      init = {
-        defaultBranch = "master";
-      };
+      merge.conflictstyle = "zdiff3";
+      pull.rebase = true;
     };
   };
 }
