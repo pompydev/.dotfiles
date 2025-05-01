dc3af12b4ee7a995dd924986481b2b28832a631d
pompydev <pompydev@proton.me>
2025-05-01 16:25:36 +0900

    allow docker to use Nvidia GPU

--- modules/nixos/docker.nix
+++ modules/nixos/docker.nix
@@ -10,8 +10,22 @@
     docker
     docker-buildx
     docker-compose
+    nvidia-docker
     lazydocker
   ];
 
-  virtualisation.docker.enable = true;
+  # https://github.com/nix-community/NixOS-WSL/discussions/487#discussioncomment-11180666
+  virtualisation.docker = {
+    enable = true;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+      daemon.settings = {
+        features.cdi = true;
+        cdi-spec-dirs = [ "/home/${user}/.cdi" ];
+      };
+    };
+    daemon.settings.features.cdi = true;
+  };
+  hardware.nvidia-container-toolkit.enable = true;
 }
