c2b986c6e65448e9da2ed82fb1d1d339a0964dcf
pompydev <pompydev@proton.me>
2025-05-21 13:22:51 +0900

    simplify devenv

--- devenv.nix
+++ devenv.nix
@@ -3,13 +3,8 @@
 {
   packages = with pkgs; [
     just
+
+    nixd
     nixfmt-rfc-style
   ];
-
-  # https://github.com/cachix/devenv/blob/main/src/modules/languages/nix.nix
-  # https://devenv.sh/supported-languages/nix
-  languages.nix = {
-    enable = true;
-    lsp.package = pkgs.nixd;
-  };
 }
